3c8c0ec509f02f723f512ff6229014e02146a2a9
ReySinez <91934375+ReySinez@users.noreply.github.com>
2023-06-21 21:18:46 +0300

    Delete goit-js-hw-06/src/src directory

--- goit-js-hw-06/src/src/js/task-05.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const nameInput = document.getElementById('name-input');
-  const nameOutput = document.getElementById('name-output');
-
-  nameInput.addEventListener('input', function() {
-    nameOutput.textContent = nameInput.value || 'Anonymous';
-  });
-});
